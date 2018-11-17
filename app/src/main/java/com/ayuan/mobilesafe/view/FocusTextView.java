27e60cf456f872e3ea32995921b597a0405c3345
lzy21 <lzy2118053626@hotmail.com>
2018-11-17 16:08:04 +0800

    change

--- app/src/main/java/com/ayuan/mobilesafe/view/FocusTextView.java
+++ app/src/main/java/com/ayuan/mobilesafe/view/FocusTextView.java
@@ -6,6 +6,10 @@ import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.widget.TextView;
 
+/**
+ * @author ayuan
+ * 能够获取焦点的自定义TexiView
+ */
 @SuppressLint("AppCompatCustomView")
 public class FocusTextView extends TextView {
     /**
@@ -36,5 +40,15 @@ public class FocusTextView extends TextView {
      */
     public FocusTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+    }
+
+    /**
+     * 重写获取焦点的方法,由系统调用，调用的时候默认就能获取焦点
+     *
+     * @return TRUE表示能够过去焦点
+     */
+    @Override
+    public boolean isFocused() {
+        return true;
     }
 }
