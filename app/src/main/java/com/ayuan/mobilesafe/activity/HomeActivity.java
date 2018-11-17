84aa1815bf84dfd081271c61997767ac47af3890
lzy21 <lzy2118053626@hotmail.com>
2018-11-17 15:34:16 +0800

    change

--- app/src/main/java/com/ayuan/mobilesafe/activity/HomeActivity.java
+++ app/src/main/java/com/ayuan/mobilesafe/activity/HomeActivity.java
@@ -9,6 +9,5 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
     }
 }
