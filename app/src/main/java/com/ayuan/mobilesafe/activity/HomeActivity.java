8f6c6c67f5689fcb6648efee1e71cd16f9905803
lzy21 <lzy2118053626@hotmail.com>
2018-11-21 15:01:35 +0800

    change

--- app/src/main/java/com/ayuan/mobilesafe/activity/HomeActivity.java
+++ app/src/main/java/com/ayuan/mobilesafe/activity/HomeActivity.java
@@ -2,6 +2,7 @@ package com.ayuan.mobilesafe.activity;
 
 import android.app.AlertDialog;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -9,6 +10,9 @@ import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.CycleInterpolator;
+import android.view.animation.TranslateAnimation;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.Button;
@@ -119,7 +123,61 @@ public class HomeActivity extends AppCompatActivity {
      * 弹出确认密码的对话框
      */
     private void showConfirmPasswordDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        final AlertDialog alertDialog = builder.create();
+        final View inflate = View.inflate(this, R.layout.dialog_confrim_password, null);
+        alertDialog.setView(inflate);
+        alertDialog.show();
+        Button btn_submit = (Button) inflate.findViewById(R.id.btn_submit);
+        Button btn_cancel = (Button) inflate.findViewById(R.id.btn_cancel);
+        //为确认按钮设置点击事件
+        btn_submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //取得对话框输入的密码
+                EditText et_set_password = (EditText) inflate.findViewById(R.id.et_set_password);
+                String set_password = et_set_password.getText().toString().trim();
+                //从文件中获取密码
+                String repassword = SpUtils.getString(HomeActivity.this, ConstantValue.MOBILE_SAFE_PASSWORD, "");
+                if (TextUtils.isEmpty(set_password)) {
+                    //为输入框设置提示内容
+                    et_set_password.setHint("请输入密码");
+                    //将提示内容设置为红色
+                    et_set_password.setHintTextColor(Color.RED);
+                    //为输入框设置动效（左右晃动）
+                    Animation translateAnimation = new TranslateAnimation(0, 10, 0, 0);
+                    translateAnimation.setInterpolator(new CycleInterpolator(5));
+                    translateAnimation.setDuration(200);
+                    et_set_password.startAnimation(translateAnimation);
+                    return;
+                } else if (!set_password.equals(repassword)) {
+                    //将输入框置为空值
+                    et_set_password.setText("");
+                    //为输入框设置提示内容
+                    et_set_password.setHint("密码错误");
+                    et_set_password.setHintTextColor(Color.RED);
+                    //为输入框设置动效（左右晃动）
+                    Animation translateAnimation = new TranslateAnimation(0, 10, 0, 0);
+                    translateAnimation.setInterpolator(new CycleInterpolator(5));
+                    translateAnimation.setDuration(200);
+                    et_set_password.startAnimation(translateAnimation);
+                    return;
+                } else {
+                    //密码输入正确
+                    Intent intent = new Intent(getApplicationContext(), TestActvity.class);
+                    startActivity(intent);
+                    alertDialog.dismiss();
+                }
+            }
+        });
 
+        //为取消按钮设置点击事件
+        btn_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alertDialog.dismiss();
+            }
+        });
     }
 
     /**
@@ -147,17 +205,34 @@ public class HomeActivity extends AppCompatActivity {
                 String confirmPassword = et_confirm_password.getText().toString().trim();
                 if (!TextUtils.isEmpty(setPassword) && !TextUtils.isEmpty(confirmPassword) && setPassword.equals(confirmPassword)) {
                     Log.i(TAG, "成功开启页面");
-                    //进入应用手机防盗模块
+                    //将设置好的密码存起来
                     SpUtils.putString(HomeActivity.this, ConstantValue.MOBILE_SAFE_PASSWORD, setPassword);
+                    //进入应用手机防盗模块
+                    Intent intent = new Intent(getApplicationContext(), TestActvity.class);
+                    startActivity(intent);
+                    //跳转到新的界面以后需要将AlertDialog销毁
+                    alertDialog.dismiss();
                 } else if (TextUtils.isEmpty(confirmPassword) && TextUtils.isEmpty(setPassword)) {
-                    ToastUtil.showShort(HomeActivity.this, "请输入密码");
+                    et_set_password.setHint("请输入密码");
                     et_confirm_password.setHint("确认密码");
+                    et_confirm_password.setHintTextColor(Color.RED);
+                    et_set_password.setHintTextColor(Color.RED);
+                    Animation translateAnimation = new TranslateAnimation(0, 10, 0, 0);
+                    translateAnimation.setInterpolator(new CycleInterpolator(5));
+                    translateAnimation.setDuration(200);
+                    et_confirm_password.startAnimation(translateAnimation);
+                    et_set_password.startAnimation(translateAnimation);
                     return;
                 } else {
                     //设置新提示
                     et_confirm_password.setHint("两次密码输入不一致");
                     //将确认密码输入设置为空值
                     et_confirm_password.setText("");
+                    et_confirm_password.setHintTextColor(Color.RED);
+                    Animation translateAnimation = new TranslateAnimation(0, 10, 0, 0);
+                    translateAnimation.setInterpolator(new CycleInterpolator(5));
+                    translateAnimation.setDuration(200);
+                    et_confirm_password.startAnimation(translateAnimation);
                     return;
                 }
             }
