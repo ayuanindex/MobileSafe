4714285b728421f5e69d0adc37a90809eb8bc8a6
lzy21 <lzy2118053626@hotmail.com>
2018-11-21 20:13:50 +0800

    change

--- app/src/main/java/com/ayuan/mobilesafe/activity/SettingActivity.java
+++ app/src/main/java/com/ayuan/mobilesafe/activity/SettingActivity.java
@@ -1,11 +1,8 @@
 package com.ayuan.mobilesafe.activity;
 
-import android.content.ContentValues;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 
 import com.ayuan.mobilesafe.utils.ConstantValue;
