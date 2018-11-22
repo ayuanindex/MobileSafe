b3c58fdf26a0b0813ec23e51e95985fec70966d5
lzy21 <lzy2118053626@hotmail.com>
2018-11-22 20:43:09 +0800

    change

--- app/src/main/java/com/ayuan/mobilesafe/activity/SetupOneActivity.java
+++ app/src/main/java/com/ayuan/mobilesafe/activity/SetupOneActivity.java
@@ -1,8 +1,11 @@
 package com.ayuan.mobilesafe.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * 这是第一个导航界面
@@ -12,5 +15,13 @@ public class SetupOneActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup_one);
+        Button btn_next = (Button) findViewById(R.id.btn_next);
+        btn_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /*Intent intent = new Intent(SetupOneActivity.this, SetupTwoActivity.class);
+                startActivity(intent);*/
+            }
+        });
     }
 }
