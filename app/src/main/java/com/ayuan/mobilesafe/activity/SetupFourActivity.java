cc153ae556e1403d9385a17236e6c7d74c657543
lzy21 <lzy2118053626@hotmail.com>
2018-11-24 22:53:07 +0800

    change

--- app/src/main/java/com/ayuan/mobilesafe/activity/SetupFourActivity.java
+++ app/src/main/java/com/ayuan/mobilesafe/activity/SetupFourActivity.java
@@ -7,10 +7,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.Toast;
+import android.widget.CompoundButton;
 
 import com.ayuan.mobilesafe.utils.ConstantValue;
 import com.ayuan.mobilesafe.utils.SpUtils;
+import com.ayuan.mobilesafe.utils.ToastUtil;
 
 /**
  * 导航界面4
@@ -34,8 +35,29 @@ public class SetupFourActivity extends AppCompatActivity implements View.OnClick
         btn_previous = (Button) findViewById(R.id.btn_previous);
         cb_box = (CheckBox) findViewById(R.id.cb_box);
         //回显是否选中的状态
-        boolean aBoolean = SpUtils.getBoolean(this, ConstantValue.OPEN_SECURITY, false);
-        cb_box.setChecked(aBoolean);
+        boolean status = SpUtils.getBoolean(this, ConstantValue.OPEN_SECURITY, false);
+        //回显checkbox的状态
+        cb_box.setChecked(status);
+        //根据状态，修改checkbox后续文字的显示
+        if (status) {
+            cb_box.setText("安全设置已开启");
+        } else {
+            cb_box.setText("安全设置已关闭");
+        }
+        //点击过程中，监听选中状态发生改变
+        cb_box.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                //isChecked：点击后的状态
+                //存储点击后的状态
+                SpUtils.putBoolean(SetupFourActivity.this, ConstantValue.OPEN_SECURITY, isChecked);
+                if (isChecked) {
+                    cb_box.setText("安全设置已开启");
+                } else {
+                    cb_box.setText("安全设置已关闭");
+                }
+            }
+        });
 
         btn_next.setOnClickListener(this);
         btn_previous.setOnClickListener(this);
@@ -58,11 +80,15 @@ public class SetupFourActivity extends AppCompatActivity implements View.OnClick
      * 跳转到下一个页面
      */
     private void nextJump() {
-        Intent intent = new Intent(getApplicationContext(), SetupOverActivity.class);
-        startActivity(intent);
         boolean checked = cb_box.isChecked();
-        SpUtils.putBoolean(this, ConstantValue.SETUP_OVER, true);
-        finish();
+        if (checked) {
+            Intent intent = new Intent(getApplicationContext(), SetupOverActivity.class);
+            startActivity(intent);
+            SpUtils.putBoolean(this, ConstantValue.SETUP_OVER, true);
+            finish();
+        } else {
+            ToastUtil.showShort(this, "请开启防盗保护");
+        }
     }
 
     /**
