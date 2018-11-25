2e1ce543b069736e420ee1098b216d0e5270fb1c
lzy21 <lzy2118053626@hotmail.com>
2018-11-25 17:04:30 +0800

    change

--- app/src/main/java/com/ayuan/mobilesafe/activity/SetupOneActivity.java
+++ app/src/main/java/com/ayuan/mobilesafe/activity/SetupOneActivity.java
@@ -13,23 +13,23 @@ import android.widget.LinearLayout;
  * 导航界面1
  */
 public class SetupOneActivity extends AppCompatActivity {
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_setup_one);
-        Button btn_next = (Button) findViewById(R.id.btn_next);
-        btn_next.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //跳转到第二个导航界面
-                Intent intent = new Intent(SetupOneActivity.this, SetupTwoActivity.class);
-                startActivity(intent);
-                LinearLayout ll_one = (LinearLayout) findViewById(R.id.ll_one);
-                //开启平移动画
-                finish();
-                SystemClock.sleep(200);
-                overridePendingTransition(R.anim.next_in_anim, R.anim.next_out_anim);
-            }
-        });
-    }
+	@Override
+	protected void onCreate(@Nullable Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_setup_one);
+		Button btn_next = (Button) findViewById(R.id.btn_next);
+		btn_next.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				//跳转到第二个导航界面
+				Intent intent = new Intent(SetupOneActivity.this, SetupTwoActivity.class);
+				startActivity(intent);
+				LinearLayout ll_one = (LinearLayout) findViewById(R.id.ll_one);
+				//开启平移动画
+				finish();
+				SystemClock.sleep(200);
+				overridePendingTransition(R.anim.next_in_anim, R.anim.next_out_anim);
+			}
+		});
+	}
 }
